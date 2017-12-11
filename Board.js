c0875856b2d6472843c941e41c5f84f0a8db6a41
Bartosz <surzyn_b@interia.pl>
2017-12-11 17:19:18 +0100

    without console error

--- Board.js
+++ Board.js
@@ -7,23 +7,22 @@ var board = {
 	element: $('#board .column-container')
 };
 
-$('.create-column')
-	.click(function(){
-		var columnName = prompt('Enter a column name');
-		if (columnName != null) {
-			$.ajax({
-				url: baseUrl + '/column',
-				method: 'POST',
-				data: {
-					name: columnName
-				},
-				success: function(response){
-					var column = new Column(response.id, columnName);
-					board.createColumn(column);
-				}
-			});
-		}
-	});;
+$('.create-column').click(function(){
+	var columnName = prompt('Enter a column name');
+	if (columnName != null) {
+		$.ajax({
+			url: baseUrl + '/column',
+			method: 'POST',
+			data: {
+				name: columnName
+			},
+			success: function(response){
+				var column = new Column(response.id, columnName);
+				board.createColumn(column);
+			}
+		});
+	}
+});
 	
 function initSortable() {
 	$('.card-list').sortable({
